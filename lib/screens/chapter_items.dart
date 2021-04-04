b7f297daa1cae3bacba474038a2392f5c85cd434
alkaitagi <alkaitagi@outlook.com>
2021-04-04 11:04:45 +0300

    Fix

--- lib/screens/chapter_items.dart
+++ lib/screens/chapter_items.dart
@@ -6,32 +6,32 @@ class ChapterItems extends StatelessWidget {
   final Chapter chapter;
 
   @override
-  Widget build(BuildContext context) => Expanded(
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200,
-              childAspectRatio: 0.9,
-              crossAxisSpacing: 8,
-              mainAxisSpacing: 8),
-          itemCount: chapter.items.length,
-          itemBuilder: (context, index) {
-            var filename = 'assets/images/${chapter.items[index].name}.png';
-            return TextButton(
-              onPressed: () {},
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: Image.asset(filename),
-                    ),
-                    Text(chapter.items[index].name),
-                  ],
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 0.9,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 8),
+      itemCount: chapter.items.length,
+      itemBuilder: (context, index) {
+        var filename = 'assets/images/${chapter.items[index].name}.png';
+        return TextButton(
+          onPressed: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Image.asset(filename),
                 ),
-              ),
-            );
-          },
-        ),
-      );
+                Text(chapter.items[index].name),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
