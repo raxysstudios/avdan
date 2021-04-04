badc5273eb2f65e53824e18d0608579d099dcffd
alkaitagi <alkaitagi@outlook.com>
2021-04-04 23:03:04 +0300

    Refactor audio player

--- lib/audio-player.dart
+++ lib/audio-player.dart
@@ -1,3 +1,13 @@
 import 'package:just_audio/just_audio.dart';
 
 final audioPlayer = AudioPlayer();
+
+playAsset(String path) async {
+  if (audioPlayer.playing) audioPlayer.stop();
+  try {
+    await audioPlayer.setAsset(path);
+    audioPlayer.play();
+  } catch (e) {
+    print(e);
+  }
+}
