f35951c0057feb599000da6e09f275df4b83e935
alkaitagi <alkaitagi@outlook.com>
2021-04-05 01:01:47 +0300

    Language card

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,35 +9,48 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(8),
-        child: Center(
-          child: Column(
-            children: [
-              Text("Avdan", style: TextStyle(fontSize: 36)),
-              Text("by Xoxag"),
-              SizedBox(height: 8),
-              Expanded(
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                      maxCrossAxisExtent: 500,
-                      childAspectRatio: 2,
-                      crossAxisSpacing: 8,
-                      mainAxisSpacing: 8),
-                  itemCount: languages.length,
-                  itemBuilder: (context, index) => TextButton(
-                    onPressed: () {
-                      targetLanguage = languages[index]['english'] ?? '';
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ChaptersScreen()),
-                      );
-                    },
-                    child: LanguageCard(translations: languages[index]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  "Avdan",
+                  style: TextStyle(
+                    fontSize: 36,
                   ),
                 ),
+                Text(
+                  " by Xoxag",
+                  style: TextStyle(
+                    fontSize: 36,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 400,
+                    childAspectRatio: 3,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 8),
+                itemCount: languages.length,
+                itemBuilder: (context, index) => TextButton(
+                  onPressed: () {
+                    targetLanguage = languages[index]['english'] ?? '';
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ChaptersScreen()),
+                    );
+                  },
+                  child: LanguageCard(translations: languages[index]),
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/widgets/language_card.dart
+++ lib/widgets/language_card.dart
@@ -1,25 +1,52 @@
+import 'package:avdan/data/store.dart';
 import 'package:flutter/material.dart';
-
-import 'label.dart';
 
 class LanguageCard extends StatelessWidget {
   LanguageCard({required this.translations});
   final Map<String, String> translations;
 
   String get name => translations['english'] ?? '';
+  String get native => translations['native'] ?? '';
   String get image => 'assets/flags/$name.png';
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(child: Image.asset(image)),
-          Label(translations: translations),
-        ],
-      ),
+    return Stack(
+      children: [
+        Container(
+          alignment: Alignment.centerRight,
+          child: Image.asset(
+            image,
+            fit: BoxFit.contain,
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    capitalize(native),
+                    style: TextStyle(
+                      fontSize: 28,
+                    ),
+                  ),
+                  Text(
+                    capitalize(name),
+                    style: TextStyle(
+                      fontSize: 28,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
