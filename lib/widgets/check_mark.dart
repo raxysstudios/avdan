3a4c904a9a1d6c76d82afb481039178638f847cc
alkaitagi <alkaitagi@outlook.com>
2021-04-07 10:18:58 +0300

    Optimize check mark animation

--- lib/widgets/check_mark.dart
+++ lib/widgets/check_mark.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
 
-class CheckMark extends StatefulWidget {
+class CheckMark extends StatelessWidget {
   CheckMark(this.checked);
   final bool checked;
 
   @override
-  _CheckMarkState createState() => _CheckMarkState();
-}
-
-class _CheckMarkState extends State<CheckMark> {
-  @override
   Widget build(BuildContext context) {
     return AnimatedPositioned(
-      left: widget.checked ? 0 : -32,
-      bottom: widget.checked ? 0 : -32,
+      left: checked ? 0 : -28,
+      bottom: checked ? 0 : -28,
       duration: Duration(milliseconds: 300),
       curve: standardEasing,
       child: Container(
-        width: 32,
-        height: 32,
+        width: 28,
+        height: 28,
         decoration: BoxDecoration(
           color: Colors.blue,
           borderRadius: BorderRadius.only(
