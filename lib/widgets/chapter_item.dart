e62358caccf6c0d7df262e75b99234c501951946
alkaitagi <alkaitagi@outlook.com>
2021-04-07 09:21:23 +0300

    Fix border radius

--- lib/widgets/chapter_item.dart
+++ lib/widgets/chapter_item.dart
@@ -21,7 +21,7 @@ class ChapterItem extends StatelessWidget {
     return Card(
       elevation: selected ? 2 : 1,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
+        borderRadius: BorderRadius.circular(8),
       ),
       clipBehavior: Clip.antiAlias,
       child: InkWell(
