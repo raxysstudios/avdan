2dd5f0f6b655fa08d5c9fb7ef6d4f0071b87ea93
alkaitagi <alkaitagi@outlook.com>
2021-05-20 11:39:02 +0300

    Refactor const

--- lib/widgets/selectable_card.dart
+++ lib/widgets/selectable_card.dart
@@ -4,29 +4,33 @@ class SelectableCard extends StatelessWidget {
   SelectableCard({
     required this.children,
     this.selected = false,
+    this.elevated = false,
     this.onTap,
   });
 
   final List<Widget> children;
   final bool selected;
+  final bool elevated;
   final Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
+    const size = 28.0;
     return Card(
+      elevation: elevated ? Theme.of(context).cardTheme.elevation : 0,
       child: InkWell(
         onTap: onTap,
         child: Stack(
           children: [
             ...children,
             AnimatedPositioned(
-              left: selected ? 0 : -28,
-              bottom: selected ? 0 : -28,
+              left: selected ? 0 : -size,
+              bottom: selected ? 0 : -size,
               duration: Duration(milliseconds: 250),
               curve: standardEasing,
               child: Container(
-                width: 28,
-                height: 28,
+                width: size,
+                height: size,
                 decoration: BoxDecoration(
                   color: Theme.of(context).colorScheme.primary,
                   borderRadius: BorderRadius.only(
