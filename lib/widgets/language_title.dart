4c93f550f21ecc0e6a92c73e88cee2533b8c9492
alkaitagi <alkaitagi@outlook.com>
2021-05-20 11:29:46 +0300

    Add bold subtitle

--- lib/widgets/language_title.dart
+++ lib/widgets/language_title.dart
@@ -24,6 +24,7 @@ class LanguageTitle extends StatelessWidget {
           capitalize(translatedName),
           style: TextStyle(
             fontSize: 16,
+            fontWeight: FontWeight.bold,
             color: Theme.of(context).hintColor,
           ),
         ),
