8bc15628201bfca9dd323c41dd21effac84bd3e8
alkaitagi <alkaitagi@outlook.com>
2021-08-06 02:07:30 +0300

    Simplify carts

--- lib/settings/settings_screen.dart
+++ lib/settings/settings_screen.dart
@@ -3,7 +3,6 @@ import 'package:avdan/data/language.dart';
 import 'package:avdan/data/utils.dart';
 import 'package:avdan/settings/donate_button.dart';
 import 'package:avdan/store.dart';
-import 'package:avdan/widgets/segment_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -49,7 +48,6 @@ class _SettingsScreenState extends State<SettingsScreen> {
         ),
         centerTitle: true,
       ),
-      backgroundColor: Colors.blueGrey.shade50,
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () => Navigator.pop(context),
         icon: Icon(Icons.home_outlined),
@@ -63,86 +61,87 @@ class _SettingsScreenState extends State<SettingsScreen> {
       body: ListView(
         padding: const EdgeInsets.only(bottom: 80),
         children: [
-          SegmentCard(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: Column(
-                  children: [
-                    Icon(Icons.landscape_outlined),
-                    Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Text(
-                        Localization.get('honor'),
-                        textAlign: TextAlign.center,
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  Icon(Icons.landscape_outlined),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                      Localization.get('honor'),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: DonateButton(),
                       ),
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: DonateButton(),
+                      SizedBox(width: 8),
+                      Expanded(
+                        child: ElevatedButton.icon(
+                          onPressed: () => launch('https://t.me/alkaitagi'),
+                          icon: Icon(Icons.send_outlined),
+                          label: Text(capitalize(Localization.get('contact'))),
                         ),
-                        SizedBox(width: 8),
-                        Expanded(
-                          child: ElevatedButton.icon(
-                            onPressed: () => launch('https://t.me/alkaitagi'),
-                            icon: Icon(Icons.send_outlined),
-                            label:
-                                Text(capitalize(Localization.get('contact'))),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-          SegmentCard(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: Text(
-                  capitalize(Localization.get('interface')),
-                  style: Theme.of(context).textTheme.headline6,
-                  textAlign: TextAlign.center,
+          Card(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    capitalize(Localization.get('interface')),
+                    style: Theme.of(context).textTheme.headline6,
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-              ),
-              for (final l in interface)
-                LanguageTile(
-                  l,
-                  selected: Store.interface == l,
-                  onTap: (_) {
-                    setState(() => Store.interface = l);
-                    saveChoice('interface', l);
-                  },
-                ),
-            ],
+                for (final l in interface)
+                  LanguageTile(
+                    l,
+                    selected: Store.interface == l,
+                    onTap: (_) {
+                      setState(() => Store.interface = l);
+                      saveChoice('interface', l);
+                    },
+                  ),
+              ],
+            ),
           ),
-          SegmentCard(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: Text(
-                  capitalize(Localization.get('learning')),
-                  style: Theme.of(context).textTheme.headline6,
-                  textAlign: TextAlign.center,
+          Card(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    capitalize(Localization.get('learning')),
+                    style: Theme.of(context).textTheme.headline6,
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-              ),
-              for (final l in learning)
-                LanguageTile(
-                  l,
-                  selected: Store.learning == l,
-                  alt: l.alt != null && Store.learning == l && Store.alt,
-                  onTap: (alt) {
-                    setState(() {
-                      Store.learning = l;
-                      Store.alt = alt ?? false;
-                    });
-                    saveChoice('learning', l, alt: Store.alt);
-                  },
-                ),
-            ],
+                for (final l in learning)
+                  LanguageTile(
+                    l,
+                    selected: Store.learning == l,
+                    alt: l.alt != null && Store.learning == l && Store.alt,
+                    onTap: (alt) {
+                      setState(() {
+                        Store.learning = l;
+                        Store.alt = alt ?? false;
+                      });
+                      saveChoice('learning', l, alt: Store.alt);
+                    },
+                  ),
+              ],
+            ),
           ),
         ],
       ),
