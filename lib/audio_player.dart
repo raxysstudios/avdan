b47e9731db17e8a8e71eba76c6f4ba11df87854d
alkaitagi <alkaitagi@outlook.com>
2021-09-04 22:06:01 +0300

    Fix audio path

--- lib/audio_player.dart
+++ lib/audio_player.dart
@@ -7,16 +7,17 @@ final _player = AudioPlayer();
 
 void playItem(Chapter chapter, [Translation? item]) async {
   final path = [
-    'assets',
-    'audio',
-    Store.learning.name.id,
-    chapter.title.id,
-    item == null
-        ? chapter.title.id
-        : chapter.alphabet
-            ? item.map[Store.learning.alt]
-            : item.id,
-  ].join('/');
+        'assets',
+        'audio',
+        Store.learning.name.id,
+        chapter.title.id,
+        item == null
+            ? chapter.title.id
+            : chapter.alphabet
+                ? item.map[Store.learning.alt]
+                : item.id,
+      ].join('/') +
+      '.mp3';
 
   try {
     if (_player.playing) await _player.stop();
