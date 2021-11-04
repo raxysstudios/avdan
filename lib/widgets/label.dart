06b8de80e611133c221aa2f0190439cb0fbcd800
alkaitagi <alkaitagi@outlook.com>
2021-11-04 14:02:44 +0300

    Remove custom fonts

--- lib/widgets/label.dart
+++ lib/widgets/label.dart
@@ -1,7 +1,6 @@
 import 'package:avdan/data/translation.dart';
 import 'package:avdan/store.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class Label extends StatelessWidget {
@@ -24,7 +23,6 @@ class Label extends StatelessWidget {
     return RichText(
       textAlign: TextAlign.center,
       text: TextSpan(
-        style: GoogleFonts.robotoSlab(),
         children: [
           TextSpan(
             text: learning,
