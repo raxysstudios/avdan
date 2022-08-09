2f838e499d6353909faa620af0bb0dae5418ea14
alkaitagi <alkaitagi@outlook.com>
2022-08-09 16:32:21 +0300

    Clean up store

--- lib/store.dart
+++ lib/store.dart
@@ -1,39 +1,30 @@
 import 'dart:convert';
+import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'models/chapter.dart';
 import 'models/language.dart';
 import 'shared/extensions.dart' as cap;
 
 typedef Dict<T> = Map<String, T>;
 
 class Store with ChangeNotifier {
-  Store();
+  Directory? _cache;
   late SharedPreferences _prefs;
-  late Dict<Dict<String>> _localization;
+
   String localize(String key, [bool capitalize = true]) {
-    var text = _localization[key]?[interface.name.id] ?? '';
-    if (capitalize) {
-      text = cap.capitalize(text);
-    }
+    var text = interface.localization[key] ?? '';
+    if (capitalize) text = cap.capitalize(text);
     return text;
   }
-
-  late List<Language> _languages;
-  List<Language> get languages => _languages;
-
-  late List<Chapter> _chapters;
-  List<Chapter> get chapters => _chapters;
 
   late Language _interface;
   Language get interface => _interface;
   set interface(Language val) {
     _interface = val;
-    _prefs.setString('interface', _interface.name.id);
+    _prefs.setString('interface', json.encode(val.toJson()));
     notifyListeners();
   }
 
@@ -41,7 +32,7 @@ class Store with ChangeNotifier {
   Language get learning => _learning;
   set learning(Language val) {
     _learning = val;
-    _prefs.setString('learning', _learning.name.id);
+    _prefs.setString('learning', json.encode(val.toJson()));
     notifyListeners();
   }
 
@@ -54,6 +45,7 @@ class Store with ChangeNotifier {
   }
 
   Future<void> load() async {
+    if (kIsWeb) _cache = await getApplicationDocumentsDirectory();
     _prefs = await SharedPreferences.getInstance();
     _alt = _prefs.getBool('alt') ?? false;
 
