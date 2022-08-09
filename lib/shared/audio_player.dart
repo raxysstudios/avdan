6fe6e1db4713bbb1e6a7473d4edd61878ffb14e4
alkaitagi <alkaitagi@outlook.com>
2022-08-09 16:47:52 +0300

    Simplify audio player

--- lib/shared/audio_player.dart
+++ lib/shared/audio_player.dart
@@ -1,28 +1,8 @@
-import 'package:avdan/models/chapter.dart';
-import 'package:avdan/models/language.dart';
-import 'package:avdan/models/translation.dart';
 import 'package:just_audio/just_audio.dart';
 
 var _player = AudioPlayer();
 
-void playItem(
-  Language language,
-  Chapter chapter, [
-  Translation? item,
-]) async {
-  final name = item == null
-      ? chapter.id
-      : chapter.alphabet
-          ? item.get(language.alt) ?? item.get(language.id)
-          : item.id;
-  final url = [
-    'assets',
-    'audio',
-    language.id,
-    chapter.title.id,
-    '$name.mp3',
-  ].join('/');
-
+void play(String url) async {
   try {
     await _player.dispose();
     final player = AudioPlayer();
