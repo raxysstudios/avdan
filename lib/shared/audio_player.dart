13de66dc06080d02a9ddbd95056f5fef933905f2
alkaitagi <alkaitagi@outlook.com>
2022-08-12 22:42:57 +0300

    Install packages & fix audio playback

--- lib/shared/audio_player.dart
+++ lib/shared/audio_player.dart
@@ -1,8 +1,10 @@
 import 'dart:typed_data';
 
+// import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:avdan/models/card.dart' as avd;
 import 'package:avdan/store.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:provider/provider.dart';
 
@@ -15,10 +17,16 @@ void playCard(BuildContext context, avd.Card card) async {
     _player = player;
 
     final audio = context.read<Store>().media.get(card.audioPath);
-    if (audio != null) {
-      await player.setAudioSource(BufferAudioSource(audio));
-      await player.play();
-    }
+    // final p = AssetsAudioPlayer();
+    // p.open(Audio.liveStream(path))
+    final p = FlutterSoundPlayer();
+    await p.openPlayer();
+    p.startPlayer(fromDataBuffer: audio);
+
+    // if (audio != null) {
+    //   await player.setAudioSource(BufferAudioSource(audio));
+    //   await player.play();
+    // }
   } catch (e) {
     rethrow;
   }
@@ -32,7 +40,6 @@ class BufferAudioSource extends StreamAudioSource {
   Future<StreamAudioResponse> request([int? start, int? end]) {
     start = start ?? 0;
     end = end ?? _buffer.length;
-
     return Future.value(
       StreamAudioResponse(
         sourceLength: _buffer.length,
