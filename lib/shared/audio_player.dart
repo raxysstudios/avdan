c941e38afa3a4e54c0637745df2d99fa3e22071a
alkaitagi <alkaitagi@outlook.com>
2022-08-14 00:15:14 +0300

    Clean up audio player

--- lib/shared/audio_player.dart
+++ lib/shared/audio_player.dart
@@ -1,57 +1,19 @@
-import 'dart:typed_data';
-
-// import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:avdan/models/card.dart' as avd;
 import 'package:avdan/store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
-import 'package:just_audio/just_audio.dart';
 import 'package:provider/provider.dart';
 
-var _player = AudioPlayer();
+final _player = FlutterSoundPlayer();
 
 void playCard(BuildContext context, avd.Card card) async {
   try {
-    await _player.dispose();
-    final player = AudioPlayer();
-    _player = player;
-
-    final audio = context.read<Store>().media.get(card.audioPath);
-    // final p = AssetsAudioPlayer();
-    // p.open(Audio.liveStream(path))
-    final p = FlutterSoundPlayer();
-    await p.openPlayer();
-    p.startPlayer(fromDataBuffer: audio);
-
-    // if (audio != null) {
-    //   await player.setAudioSource(BufferAudioSource(audio));
-    //   await player.play();
-    // }
+    if (!_player.isOpen()) await _player.openPlayer();
+    _player.stopPlayer();
+    _player.startPlayer(
+      fromDataBuffer: context.read<Store>().media.get(card.audioPath),
+    );
   } catch (e) {
     rethrow;
-  }
-}
-
-class BufferAudioSource extends StreamAudioSource {
-  BufferAudioSource(this._buffer) : super(tag: 'Bla');
-  final Uint8List _buffer;
-
-  @override
-  Future<StreamAudioResponse> request([int? start, int? end]) {
-    start = start ?? 0;
-    end = end ?? _buffer.length;
-    return Future.value(
-      StreamAudioResponse(
-        sourceLength: _buffer.length,
-        contentLength: end - start,
-        offset: start,
-        contentType: 'audio/mpeg',
-        stream: Stream.value(
-          List<int>.from(
-            _buffer.skip(start).take(end - start),
-          ),
-        ),
-      ),
-    );
   }
 }
