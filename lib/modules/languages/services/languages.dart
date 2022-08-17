87982bd810b252ffe4c3490f37391a79e0013e37
alkaitagi <alkaitagi@outlook.com>
2022-08-17 15:51:16 +0300

    Clean up

--- lib/modules/languages/services/languages.dart
+++ lib/modules/languages/services/languages.dart
@@ -1,15 +1,14 @@
 import 'package:avdan/models/language.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-Future<List<Language>> fetchLanguages() async {
-  final query = FirebaseFirestore.instance
+Future<List<Language>> fetchLanguages() {
+  return FirebaseFirestore.instance
       .collection('languages')
       .withConverter(
         fromFirestore: (s, _) => Language.fromJson(s.data()!),
         toFirestore: (_, __) => {},
       )
-      .orderBy('name');
-
-  final snap = await query.get();
-  return snap.docs.map((d) => d.data()).toList();
+      .orderBy('name')
+      .get()
+      .then((s) => s.docs.map((d) => d.data()).toList());
 }
