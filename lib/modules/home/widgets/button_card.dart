d476d209b9f897a3fb463e98fafcab34d93436fd
alkaitagi <alkaitagi@outlook.com>
2022-09-01 16:34:35 +0300

    Add elevation change on tap for button card

--- lib/modules/home/widgets/button_card.dart
+++ lib/modules/home/widgets/button_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ButtonCard extends StatelessWidget {
+class ButtonCard extends StatefulWidget {
   const ButtonCard({
     required this.onTap,
     required this.child,
@@ -10,19 +10,32 @@ class ButtonCard extends StatelessWidget {
   final Widget child;
 
   @override
+  State<ButtonCard> createState() => _ButtonCardState();
+}
+
+class _ButtonCardState extends State<ButtonCard> {
+  var isHover = false;
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       shape: const StadiumBorder(),
-      elevation: 2,
+      elevation: 2 * (isHover ? 3 : 1),
       margin: const EdgeInsets.all(8),
       child: InkWell(
-        onTap: onTap,
+        onTap: widget.onTap,
+        onTapDown: (_) => setState(() {
+          isHover = true;
+        }),
+        onTapCancel: () => setState(() {
+          isHover = false;
+        }),
         child: Padding(
           padding: const EdgeInsets.symmetric(
             vertical: 12,
             horizontal: 16,
           ),
-          child: child,
+          child: widget.child,
         ),
       ),
     );
