45a254b59ed252833bfc38097a956b1ff049d0d2
alkaitagi <alkaitagi@outlook.com>
2025-06-20 11:03:21 +0300

    Rework prefs into a static class

--- lib/shared/prefs.dart
+++ lib/shared/prefs.dart
@@ -1,25 +1,50 @@
+import 'dart:convert';
+
+import 'package:avdan/models/language.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-late final Box<dynamic> _prefs;
+class Prefs {
+  Prefs._();
 
-Future<void> initPrefs() async {
-  _prefs = await Hive.openBox<dynamic>('prefs');
+  static late final Box<dynamic> _prefs;
+
+  static Future<void> init() async {
+    _prefs = await Hive.openBox<dynamic>('prefs');
+  }
+
+  static String get interfaceLanguage => _prefs.get(
+        'locale',
+        defaultValue: '',
+      );
+  static set interfaceLanguage(String v) => _prefs.put('locale', v);
+
+  static Language? _learningLanguage;
+  static Language? get learningLanguage {
+    if (_learningLanguage == null) {
+      final json = _prefs.get('language');
+      if (json == null) return null;
+      _learningLanguage = Language.fromJson(jsonDecode(json));
+    }
+    return _learningLanguage;
+  }
+
+  static set learningLanguage(Language? v) {
+    _learningLanguage = v;
+    _prefs.put(
+      v == null ? null : jsonEncode(v.toJson()),
+      v,
+    );
+  }
+
+  static bool get altScript => _prefs.get(
+        'altScript',
+        defaultValue: false,
+      );
+  static set altScript(bool v) => _prefs.put('altScript', v);
+
+  static DateTime get lastReadNews => _prefs.get(
+        'lastReadNews',
+        defaultValue: DateTime(0),
+      );
+  static set lastReadNews(DateTime v) => _prefs.put('lastReadNews', v);
 }
-
-String get lrnLng => _prefs.get('lrnLng') as String? ?? '';
-set lrnLng(String v) => _prefs.put('lrnLng', v);
-
-DateTime get lrnUpd => _prefs.get('lrnUpd') as DateTime? ?? DateTime(0);
-set lrnUpd(DateTime? v) => _prefs.put('lrnUpd', v);
-
-String get intLng => _prefs.get('intLng') as String? ?? '';
-set intLng(String v) => _prefs.put('intLng', v);
-
-DateTime get intUpd => _prefs.get('intUpd') as DateTime? ?? DateTime(0);
-set intUpd(DateTime? v) => _prefs.put('intUpd', v);
-
-DateTime get pstUpd => _prefs.get('pstUpd') as DateTime? ?? DateTime(0);
-set pstUpd(DateTime? v) => _prefs.put('pstUpd', v);
-
-bool get isAlt => _prefs.get('isAlt') as bool? ?? false;
-set isAlt(bool v) => _prefs.put('isAlt', v);
